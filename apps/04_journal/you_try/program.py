7d4185bd74d69afcb81a24b73b3d72abb66a6a1c
Danish O'Habib <DohabiB@gmail.com>
2016-05-15 14:13:55 -0400

    Finished journal app.

--- apps/04_journal/you_try/program.py
+++ apps/04_journal/you_try/program.py
@@ -1,3 +1,7 @@
+import journal
+
+
+
 def main():
 	print_header()
 	run_event_loop()
@@ -11,11 +15,12 @@ def print_header():
 
 def run_event_loop():
 	print('What do you want to do with your journal?')
-	cmd = None
-	journal_data = []
+	cmd = 'EMPTY'
+	journal_name = 'default'
+	journal_data = journal.load(journal_name)
 
 
-	while cmd != 'x':
+	while cmd != 'x' and cmd:
 		cmd = input('[L]ist entries, [A]dd an entry, E[x]it:')
 		cmd = cmd.lower().strip()
 	
@@ -23,10 +28,11 @@ def run_event_loop():
 			list_entries(journal_data)
 		elif cmd == 'a':
 			add_entry(journal_data)
-		elif cmd != 'x':
+		elif cmd != 'x' and cmd:
 			print("Sorry, I do not understand '{}'.".format(cmd))
 	
-	print('Done, goodbye.') 		
+	print('Done, goodbye.') 	
+	journal.save(journal_name, journal_data)	
 
 def list_entries(data):
 	print('Your journal entries: ')
@@ -36,7 +42,12 @@ def list_entries(data):
 
 def add_entry(data):
 	text = input('Type your entry, <enter> to exit: ')
-	data.append(text)
+	journal.add_entry(text, data)
+	#data.append(text)
 
 
-main()
+print("__file__" + __file__)
+print("__name__" + __name__)
+
+if __name__ == '__main__':
+	main()
