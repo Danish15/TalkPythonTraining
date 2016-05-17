4c4f205a27c516269d2ba59191e91ec23955196d
Danish O'Habib <DohabiB@gmail.com>
2016-05-16 22:47:29 -0400

    Updating file

--- apps/05_weather_client/you_try/program.py
+++ apps/05_weather_client/you_try/program.py
@@ -1,4 +1,12 @@
-print("------------------------------------")
-print("				WEATHER APP			   ")
-print("------------------------------------")
+def main()
+	print_the_header
+	code = input('What zipcode do you want the weather for (97201)? ')
+	print(code)
 
+def print_the_header():
+	print("------------------------------------")
+	print("				WEATHER APP			   ")
+	print("------------------------------------")
+
+
+if __name__ == '__main__":
