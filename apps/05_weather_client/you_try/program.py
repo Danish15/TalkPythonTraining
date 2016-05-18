af151adb6f0ecfa59d614666f6f7b78c2dffc332
Danish O'Habib <DohabiB@gmail.com>
2016-05-18 15:14:14 -0400

    Updating file

--- apps/05_weather_client/you_try/program.py
+++ apps/05_weather_client/you_try/program.py
@@ -1,7 +1,13 @@
-def main()
+import requests
+import bs4
+
+def main():
 	print_the_header
 	code = input('What zipcode do you want the weather for (97201)? ')
-	print(code)
+	html = get_html_from_web(code)
+
+	get_weather_from_html(html)
+
 
 def print_the_header():
 	print("------------------------------------")
@@ -10,8 +16,16 @@ def print_the_header():
 
 
 def get_html_from_web(zipcode):
-	url = 'http"//www.wunderground.com/weather-forecast/{}'.format(zipcode)
-	requests
+	url = 'http:"//www.wunderground.com/weather-forecast/{}'.format(zipcode)
+	response = requests.get(url)
+	#print(response.text[0:250])
+
+	return response.txt
+
+def get_weather_from_html(html):
+	soup = bs4.BeautifulSoup(html, 'html.parser')
+	print(soup)
+
 
 
 
