d3d2640a31722f671bc25114c4bd57ed2a749c46
Danish O'Habib <DohabiB@gmail.com>
2016-05-18 15:34:19 -0400

    Updating file

--- apps/05_weather_client/you_try/program.py
+++ apps/05_weather_client/you_try/program.py
@@ -5,7 +5,6 @@ def main():
 	print_the_header
 	code = input('What zipcode do you want the weather for (97201)? ')
 	html = get_html_from_web(code)
-
 	get_weather_from_html(html)
 
 
@@ -16,18 +15,22 @@ def print_the_header():
 
 
 def get_html_from_web(zipcode):
-	url = 'http:"//www.wunderground.com/weather-forecast/{}'.format(zipcode)
+	url = 'http://www.wunderground.com/weather-forecast/{}'.format(zipcode)
 	response = requests.get(url)
+
+	return response.text
 	#print(response.text[0:250])
 
-	return response.txt
+	
 
 def get_weather_from_html(html):
 	soup = bs4.BeautifulSoup(html, 'html.parser')
-	print(soup)
+	loc = soup.find(id='location').find('h1').get_text()
+	condition = soup.find(id='curCond').find(class_='wx-value').get_text()
+	temp = soup.find(id='curTemp').find(class_='wx-value').get_text()
+	scale = soup.find(id='curTemp').find(class_='wx-unit').get_text()
 
-
-
+	print(condition, temp, scale)
 
 if __name__ == '__main__':
 	main()
